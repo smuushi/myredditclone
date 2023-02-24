b0e9ee20f553abe9e04f2c31fb8ac9ecc78d7562
smuushi <mishih780@gmail.com>
2023-02-24 14:14:19 -0500

    working well

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,52 @@ class UsersController < ApplicationController
     render :index
   end
 
+  def show
+    @user = User.find_by(id: params[:id])
+
+
+    # if nil user, render error messages and redirects to user index page
+
+    if @user 
+      render :show
+    else
+      flash[:errors] = ['User does not exist']
+      redirect_to users_url
+    end
+
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+    # debugger?
+    if @user.save
+      render :show
+    else
+      flash[:errors] = @user.errors.full_messages #["password should be at least 4 characterssss"]
+      render :new
+    end
+    # debugger
+
+  end
+
+
+
+  def user_params
+
+    params.require(:user).permit(:username, :password)
+
+  end
+
+  # def auth_token
+  #   debugger
+  #   "<input type='hidden' name='authenticity_token' value='#{form_authenticity_token}'>".html_safe
+
+  # end 
   
 
 
